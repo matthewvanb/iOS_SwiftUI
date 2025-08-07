c02771a2b97f8ee43d27a32a4b3de680793ebf80
matthewvanb <pistes.moody1s@icloud.com>
2025-08-06 18:25:00 -0700

    Initial Commit

---
+++ iOS_SwiftUI/iOS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_SwiftUIApp.swift
+//  iOS_SwiftUI
+//
+//  Created by Matthew Van Blaricom on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
